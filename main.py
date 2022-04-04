f4f05c7b21b35c55a1c156a62a91064af6226630
Kilulu31 <102540091+Kilulu31@users.noreply.github.com>
2022-04-04 17:57:23 +0200

    Mise à jour main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -3,19 +3,28 @@ def on_received_number(receivedNumber):
     if receivedNumber == 1:
         if chifumi == 2:
             gagnant = gagnant + 1
+            basic.pause(500)
+            basic.clear_screen()
     if receivedNumber == 2:
         if chifumi == 3:
             gagnant = gagnant + 1
+            basic.pause(500)
+            basic.clear_screen()
     if receivedNumber == 3:
         if chifumi == 1:
             gagnant = gagnant + 1
+            basic.pause(500)
+            basic.clear_screen()
+    if receivedNumber == 4:
+        basic.show_string("TU ES VRAIMANT NUL")
 radio.on_received_number(on_received_number)
 
 def on_button_pressed_a():
     global chifumi
+    radio.send_number(0)
     chifumi = randint(1, 3)
     radio.send_number(chifumi)
-    basic.pause(2000)
+    basic.pause(1000)
     chifumi = 0
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
@@ -24,7 +33,8 @@ chifumi = 0
 radio.set_group(2)
 
 def on_forever():
-    if gagnant == 10:
+    if gagnant == 6:
+        radio.send_number(4)
         basic.show_string("bien jouer")
 basic.forever(on_forever)
 
--- main.ts
+++ main.ts
@@ -2,30 +2,41 @@ radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         if (chifumi == 2) {
             gagnant = gagnant + 1
+            basic.pause(500)
+            basic.clearScreen()
         }
     }
     if (receivedNumber == 2) {
         if (chifumi == 3) {
             gagnant = gagnant + 1
+            basic.pause(500)
+            basic.clearScreen()
         }
     }
     if (receivedNumber == 3) {
         if (chifumi == 1) {
             gagnant = gagnant + 1
+            basic.pause(500)
+            basic.clearScreen()
         }
+    }
+    if (receivedNumber == 4) {
+        basic.showString("TU ES VRAIMANT NUL")
     }
 })
 input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(0)
     chifumi = randint(1, 3)
     radio.sendNumber(chifumi)
-    basic.pause(2000)
+    basic.pause(1000)
     chifumi = 0
 })
 let gagnant = 0
 let chifumi = 0
 radio.setGroup(2)
 basic.forever(function () {
-    if (gagnant == 10) {
+    if (gagnant == 6) {
+        radio.sendNumber(4)
         basic.showString("bien jouer")
     }
 })
@@ -38,6 +49,14 @@ basic.forever(function () {
             . # # # .
             . # # # .
             `)
+        basic.pause(450)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
     }
     if (chifumi == 2) {
         basic.showLeds(`
@@ -47,6 +66,14 @@ basic.forever(function () {
             # # . # .
             # # . . #
             `)
+        basic.pause(450)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
     }
     if (chifumi == 3) {
         basic.showLeds(`
@@ -55,6 +82,14 @@ basic.forever(function () {
             # # # # #
             # # # # #
             . # # # .
+            `)
+        basic.pause(450)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
             `)
     }
 })
